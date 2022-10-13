55a31387936894c2657ca84b184313e34535e252
sasankap742 <78668680+sasankap742@users.noreply.github.com>
2022-10-13 09:50:11 -0700

    arrayReversed

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -15,4 +18,19 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  @Test
+  public void testReverseInPlace3Items(){
+    int[] input1 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3, 2, 1 }, input1);
+  }
+  
+  @Test
+  public void testReversed4Items(){
+    int[] input1 = {1, 2, 3, 4};
+    assertArrayEquals(new int[]{4, 3, 2, 1 }, ArrayExamples.reversed(input1));
+  }
+  
+
+
 }
